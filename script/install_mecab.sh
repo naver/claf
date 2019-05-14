3b30444ca5a314c78f3fcb93fd21215b6373fad9
DongjunLee <humanbrain.djlee@gmail.com>
2019-05-14 20:18:28 +0900

    bugfix: add install_mecab (kor) script

---
+++ script/install_mecab.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+OUT_DIR="${1:-./mecab}"
+
+mkdir -v -p $OUT_DIR
+
+apt-get install git cmake make automake wget
+
+wget https://bitbucket.org/eunjeon/mecab-ko/downloads/mecab-0.996-ko-0.9.2.tar.gz
+wget https://bitbucket.org/eunjeon/mecab-ko-dic/downloads/mecab-ko-dic-2.1.1-20180720.tar.gz
+
+mv mecab-0.996-ko-0.9.2.tar.gz "$OUT_DIR/"
+mv mecab-ko-dic-2.1.1-20180720.tar.gz "$OUT_DIR/"
+
+cd "$OUT_DIR"
+
+tar -zxfv mecab-0.996-ko-0.9.2.tar.gz
+cd mecab-0.996-ko-0.9.2
+./configure
+make
+make check
+make install
+cd ../
+
+ldconfig
+tar -zxfv mecab-ko-dic-2.1.1-20180720.tar.gz
+cd mecab-ko-dic-2.1.1-20180720
+./configure
+make
+make install
+
+pip install mecab-python3
