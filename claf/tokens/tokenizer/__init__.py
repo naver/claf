a2a70fbaf13d5cdd398e998a8955c5e59b3e7fd5
DongjunLee <humanbrain.djlee@gmail.com>
2019-08-30 21:17:20 +0900

    feat(tokenizer): add Bype-Pair Encoding Tokenizer (roberta)

--- claf/tokens/tokenizer/__init__.py
+++ claf/tokens/tokenizer/__init__.py
@@ -1,10 +1,11 @@
 
 from .pass_text import PassText
 
+from .bpe import BPETokenizer
 from .char import CharTokenizer
 from .subword import SubwordTokenizer
 from .word import WordTokenizer
 from .sent import SentTokenizer
 
 
-__all__ = ["PassText", "CharTokenizer", "SubwordTokenizer", "WordTokenizer", "SentTokenizer"]
+__all__ = ["PassText", "BPETokenizer", "CharTokenizer", "SubwordTokenizer", "WordTokenizer", "SentTokenizer"]
